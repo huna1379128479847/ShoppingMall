5c2170b95cf4affc916578796470697911fb1793
kuzuboshi <Human.kagoshima.IdeRyouta@gmail.com>
2024-11-29 18:03:21 +0900

    itemcheck作成

---
+++ Assets/ID/Scripts/ItemCheck.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemCheck
+{
+    private static bool item_Key = false;
+    private static bool item_Camera = false;
+    private static bool item_DelayClock = false;
+    private static bool item_Accelerometer = false;
+    private static bool item_Throwing_Support = false;
+    private static bool item_Loudspeaker = false;
+    private static bool item_Salt = false;
+    private static bool item_A_cushion_that_ruins_people = false;
+    private static bool item_Metal_bat = false;
+    private static bool item_Robot_Parts = false;
+    private static bool item_Boss_info = false;
+
+    public static bool Item_Key
+    {
+        get
+        {
+            return item_Key;
+        }
+        set
+        {
+            item_Key = value;
+        }
+    }
+
+    public static bool Item_Camera
+    {
+        get
+        {
+            return item_Camera;
+        }
+        set
+        {
+            item_Camera = value;
+        }
+    }
+
+    public static bool Item_DelayClock
+    {
+        get
+        {
+            return item_DelayClock;
+        }
+        set
+        {
+            item_DelayClock = value;
+        }
+    }
+
+    public static bool Item_Accelerometer
+    {
+        get
+        {
+            return item_Accelerometer;
+        }
+        set
+        {
+            item_Accelerometer = value;
+        }
+    }
+
+    public static bool Item_Throwing_Support
+    {
+        get
+        {
+            return item_Throwing_Support;
+        }
+        set
+        {
+            item_Throwing_Support = value;
+        }
+    }
+
+    public static bool Item_Loudspeaker
+    {
+        get
+        {
+            return item_Loudspeaker;
+        }
+        set
+        {
+            item_Loudspeaker = value;
+        }
+    }
+
+    public static bool Item_Salt
+    {
+        get
+        {
+            return item_Salt;
+        }
+        set
+        {
+            item_Salt = value;
+        }
+    }
+
+    public static bool Item_A_cushion_that_ruins_people
+    {
+        get
+        {
+            return item_A_cushion_that_ruins_people;
+        }
+        set
+        {
+            item_A_cushion_that_ruins_people = value;
+        }
+    }
+
+    public static bool Item_Metal_bat
+    {
+        get
+        {
+            return item_Metal_bat;
+        }
+        set
+        {
+            item_Metal_bat = value;
+        }
+    }
+
+    public static bool Item_Robot_Parts
+    {
+        get
+        {
+            return item_Robot_Parts;
+        }
+        set
+        {
+            item_Robot_Parts = value;
+        }
+    }
+
+    public static bool Item_Boss_info
+    {
+        get
+        {
+            return item_Boss_info;
+        }
+        set
+        {
+            item_Boss_info = value;
+        }
+    }
+}
