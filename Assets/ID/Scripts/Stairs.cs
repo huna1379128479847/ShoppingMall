e0589bc006df0ea0bc9a2fc3e241c1c0c5e54d34
kuzuboshi <Human.kagoshima.IdeRyouta@gmail.com>
2024-12-06 16:32:59 +0900

    階段遷移 制作途中

--- Assets/ID/Scripts/Stairs.cs
+++ Assets/ID/Scripts/Stairs.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Stairs : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    // プレイヤーの次の出現位置を保持する静的変数
+    public static Vector3 playerSpawnPosition;
 
+    // 階段に触れたとき
     void Update()
     {
         
