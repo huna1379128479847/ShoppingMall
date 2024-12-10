b3239df04be10b005c1daebade8cb7fe7204f730
kuzuboshi <Human.kagoshima.IdeRyouta@gmail.com>
2024-12-10 16:50:28 +0900

    Stairsスクリプト 完成

--- Assets/ID/Scripts/Stairs.cs
+++ Assets/ID/Scripts/Stairs.cs
@@ -5,12 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class Stairs : MonoBehaviour
 {
-    // プレイヤーの次の出現位置を保持する静的変数
-    public static Vector3 playerSpawnPosition;
+    public Transform playerTransform;  // プレイヤーのTransform
 
-    // 階段に触れたとき
-    void Update()
+    // 階段移動メソッド
+    public void MoveToFloor()
     {
-        
+        if (CompareTag("ToSecondFloor"))  // 二階への階段に触れたとき
+        {
+            SceneManager.LoadScene("Stade2");
+            playerTransform.position = new Vector3(0.5f, 22.5f, 0f);
+        }
+
+        if(CompareTag("ToFirstFloor"))  // 一階への階段に触れたとき
+        {
+            SceneManager.LoadScene("Stade1");
+            playerTransform.position = new Vector3(6.5f, 28.5f, 0f);
+        }
+
+        if(CompareTag("ToThirdFloor"))  // 三階への階段に触れたとき
+        {
+            SceneManager.LoadScene("Stade3");
+            playerTransform.position = new Vector3(14.5f, 5.5f, 0f);
+        }
     }
 }
