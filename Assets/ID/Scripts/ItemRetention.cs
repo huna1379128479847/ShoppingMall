a7bd296a40af7e6db4160948bc963c71abe49d21
kuzuboshi <Human.kagoshima.IdeRyouta@gmail.com>
2024-12-17 17:57:05 +0900

    ItemRetentionスクリプト 修正中

--- Assets/ID/Scripts/ItemRetention.cs
+++ Assets/ID/Scripts/ItemRetention.cs
@@ -8,41 +8,23 @@ public class ItemRetention : MonoBehaviour
     // Stairsスクリプトを参照
     private Stairs stairsScript;
 
-    // アイテムタグと対応する処理を辞書で管理
-    private Dictionary<string, System.Action> itemActions;
-
     private void Start()
     {
         // シーン内のStairsコンポーネントを取得
         stairsScript = FindObjectOfType<Stairs>();
-
-        // アイテムタグと対応する処理を辞書で管理
-        itemActions = new Dictionary<string, System.Action>()
-        {
-            {"Key" , () => ItemCheck.Item_Key = true},
-            {"Camera" , () => ItemCheck.Item_Camera = true},
-            {"Alarm_clock" , () => ItemCheck.Item_Alarm_clock = true},
-            {"watch" , () => ItemCheck.Item_watch = true},
-            {"Ball" , () => ItemCheck.Item_Ball = true},
-            {"Loudspeaker" , () => ItemCheck.Item_Loudspeaker = true},
-            {"Salt" , () => ItemCheck.Item_Salt = true},
-            {"Cushion" , () => ItemCheck.Item_Cushion_that_ruins_people = true},
-            {"Metal_bat" , () => ItemCheck.Item_Metal_bat = true},
-            {"Robot_Parts" , () => ItemCheck.Item_Robot_Parts = true},
-            {"Setting_materials" , () => ItemCheck.Item_Setting_materials_collection = true},
-            {"Sweets" , () => ItemCheck.Item_Sweets = true},
-        };
     }
 
-    public void ItemGet(Collider other)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-
-            if (itemActions.ContainsKey(other.tag))
+        if (collision.gameObject.CompareTag("Player"))  // プレイヤーが触れたとき
+        {
+            if (this.gameObject.CompareTag("Key"))  // タグが"Key"の場合
             {
-                itemActions[other.tag]();  // 対応する処理を実行
-                Destroy(other.gameObject);  // アイテムを破壊
+                ItemCheck.Item_Key = true;  // 対応する処理を実行
+                DestroyAllItem();  // 他のすべてのアイテムを破壊
+                Debug.Log("アイテムを手に入れた！");
             }
-        
+        }
     }
     void Update()
     {
@@ -111,7 +93,6 @@ public class ItemRetention : MonoBehaviour
     {
         if (CompareTag("stairs"))  // 階段に触れている場合
         {
-            DestroyAllItem();  // 他のすべてのアイテムを破壊
             stairsScript.MoveToFloor();  //MoveToFloorメソッドの呼び出し
         }
     }
