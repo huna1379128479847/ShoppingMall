3690241718fd55dbfbaa6f380e14ed8f1b92ba15
kuzuboshi <Human.kagoshima.IdeRyouta@gmail.com>
2024-12-18 15:05:08 +0900

    stairsスクリプト 修正

--- Assets/ID/Scripts/Stairs.cs
+++ Assets/ID/Scripts/Stairs.cs
@@ -12,19 +12,19 @@ public class Stairs : MonoBehaviour
     {
         if (CompareTag("ToSecondFloor"))  // 二階への階段に触れたとき
         {
-            SceneManager.LoadScene("Stade2");
+            SceneManager.LoadScene("Stage2");
             playerTransform.position = new Vector3(0.5f, 22.5f, 0f);
         }
 
-        if (CompareTag("ToFirstFloor") && ItemCheck.GetItemCount() >= 1)  // 一階への階段に触れたときかつアイテムを持っていたとき
+        if (CompareTag("ToFirstFloor") && ItemCheck.GetItemCount() == 1)  // 一階への階段に触れたときかつアイテムを持っていたとき
         {
-            SceneManager.LoadScene("Stade1");
+            SceneManager.LoadScene("Stage1");
             playerTransform.position = new Vector3(6.5f, 28.5f, 0f);
         }
 
-        if (CompareTag("ToThirdFloor") && ItemCheck.GetItemCount() >= 2)  // 三階への階段に触れたときかつアイテムを持っていたとき
+        if (CompareTag("ToThirdFloor") && ItemCheck.GetItemCount() == 2)  // 三階への階段に触れたときかつアイテムを持っていたとき
         {
-            SceneManager.LoadScene("Stade3");
+            SceneManager.LoadScene("Stage3");
             playerTransform.position = new Vector3(14.5f, 5.5f, 0f);
         }
     }
